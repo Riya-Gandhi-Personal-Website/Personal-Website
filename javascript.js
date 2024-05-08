d3df4ca1d3d151f3bd5bbef124413bb33cfe3e5d
riyagandhi-2023 <riyagandhi.it@gmail.com>
2024-05-08 19:38:13 -0400

    updated url in js

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@
 
 const form = document.querySelector("#form")
 const submitButton = document.querySelector("#submit")
-const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw45bpmxzLIrUHzQFjf-VeS1Jy2WRN0XM8MB8HADQ6p9bSCD9ZRrbq_sOSeqWE2RbJNCg/exec'
 
 form.addEventListener('submit', e => {
   submitButton.disabled = true
@@ -10,13 +10,16 @@ form.addEventListener('submit', e => {
   let requestBody = new FormData(form)
   fetch(scriptURL, { method: 'POST', body: requestBody})
     .then(response => {
-       alert('Success!', response)
+      if (response.ok){
+       alert('Success!'); //only display message
+    } else {
+      throw new WebTransportError('Network response was not ok.');
+    }
        submitButton.disabled = false
       })
     .catch(error => {
-    alert('Error!', error.message)
-      submitButton.disabled = false
+    alert('Error!' + error.message);
+      submitButton.disabled = false;
 
-    }
-    )
-})
+    });
+});

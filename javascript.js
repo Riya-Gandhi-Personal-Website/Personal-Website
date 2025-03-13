d50d16698a220cf34192bded512f4738673ee7fa
riyagandhi-2023 <riyagandhi.it@gmail.com>
2025-03-12 21:46:11 -0400

    Updated resume

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@
 const form = document.querySelector("#form")
 const submitButton = document.querySelector("#submit")
 const scriptURL = 'https://script.google.com/macros/s/AKfycbw45bpmxzLIrUHzQFjf-VeS1Jy2WRN0XM8MB8HADQ6p9bSCD9ZRrbq_sOSeqWE2RbJNCg/exec'
+//'https://script.google.com/macros/s/AKfycbw6ZeQwJoRl81TcDdlaKZ3uHlFHyLqvcsFvHaAPg4W7i3qGVDweH_aIlNvIJBbU2yxVhg/exec'
 
 form.addEventListener('submit', e => {
   submitButton.disabled = true
@@ -13,7 +14,7 @@ form.addEventListener('submit', e => {
       if (response.ok){
        alert('Success!'); //only display message
     } else {
-      throw new WebTransportError('Network response was not ok.');
+      throw new Error('Network response was not ok.');
     }
        submitButton.disabled = false
       })
